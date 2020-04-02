5bc98e32561c9d4837069c9f287db39b136e4fe7
Cleianderson <cleianderson13@gmail.com>
2020-04-02 01:37:34 -0300

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'babel-plugin-root-import',
+      {
+        root: __dirname,
+        rootPathPrefix: '~/',
+        rootPathSuffix: './src/',
+      },
+    ],
+  ],
+}
