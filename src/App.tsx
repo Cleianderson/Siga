c2479b52e975cd2457cd94460723826c9412240e
Cleianderson <cleianderson13@gmail.com>
2020-04-05 08:41:00 -0300

    feat: useCallback to evite new renders

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import 'react-native-gesture-handler'
-import React, {useEffect,useState} from 'react'
+import React, {useEffect, useState, useCallback} from 'react'
+import {ThemeProvider} from 'styled-components/native'
 import {NavigationContainer} from '@react-navigation/native'
 
 import {getRealm} from './service/Realm'
 import Student from './pages/Student'
 import Login from './pages/Login'
 
+import light from './styles/themes/light'
+
 const App = () => {
   const [isLoged, setIsLoged] = useState<boolean>(false)
-  
-  const _render = () => {
+
+  const _render = useCallback(() => {
     if (isLoged) {
       return (
         <NavigationContainer>
-          <Student />
+          <ThemeProvider theme={light}>
+            <Student />
+          </ThemeProvider>
         </NavigationContainer>
       )
     }
     return <Login state={[isLoged, setIsLoged]} />
-  }
+  }, [isLoged])
 
   useEffect(() => {
     async function loadUser() {
