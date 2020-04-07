cb93c62ae6c7ad01726aee668c3a427c03dfa377
Cleianderson <cleianderson13@gmail.com>
2020-04-07 04:53:22 +0000

    feat: return Splash on initialize app

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -1,22 +1,25 @@
-type NoteSchema = {
-  id: number
-  prof: string
-  mat: string
-  VA1: string
-  VA2: string
-  VA3?: string
-  VAFN?: string
-  M: string
-  MFIN: string
-  Faltas: string
+export type NoteSchema = {
+    id: number
+    prof: string
+    mat: string
+    VA1: string
+    VA2: string
+    VA3?: string
+    VAFN?: string
+    M: string
+    MFIN: string
+    Faltas: string
 }
-type UserSchema = {
-  login: string
-  password: string
-  name: string
-  type: string
-  notes?: PeriodType[] | null
-  org?: string | null
-  model?: string | null
+export type UserSchema = {
+    login: string
+    password: string
+    name: string
+    type: string
+    notes?: PeriodType[] | null
+    org?: string | null
+    model?: string | null
 }
-type PeriodType = {name: string; subjects: NoteSchema[]}
+export type PeriodType = { name: string; subjects: NoteSchema[] }
+export type DicJSX = {
+    [key1: string]: JSX.Element
+}
