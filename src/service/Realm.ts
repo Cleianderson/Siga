3a992f38a6bb361fce93533c18ff437e43ac48f9
Cleianderson <cleianderson13@gmail.com>
2020-04-09 02:01:58 -0300

    feat: added StringArraySchema to realm

--- src/service/Realm.ts
+++ src/service/Realm.ts
@@ -4,9 +4,10 @@ import UserSchema from '../utils/Schema/UserSchema'
 import NoteSchema from '../utils/Schema/NoteSchema'
 import PeriodSchema from '../utils/Schema/PeriodSchema'
 import HorarySchema from '../utils/Schema/HorarySchema'
+import ArrayString from '../utils/Schema/StringArraySchema'
 
 export function getRealm() {
   return realm.open({
-    schema: [UserSchema, NoteSchema, PeriodSchema, HorarySchema],
+    schema: [UserSchema, NoteSchema, PeriodSchema, HorarySchema, ArrayString],
   })
 }
