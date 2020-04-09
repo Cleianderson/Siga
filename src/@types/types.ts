34b961b8ab18f7a368dd3171d4e051d405d9f78b
Cleianderson <cleianderson13@gmail.com>
2020-04-09 02:00:56 -0300

    feat: more types

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -1,14 +1,14 @@
 type NoteSchema = {
-    id: number
-    prof: string
-    mat: string
-    VA1: string
-    VA2: string
-    VA3?: string
-    VAFN?: string
-    M: string
-    MFIN: string
-    Faltas: string
+  id: number
+  prof: string
+  mat: string
+  VA1: string
+  VA2: string
+  VA3?: string
+  VAFN?: string
+  M: string
+  MFIN: string
+  Faltas: string
 }
 type UserSchema = {
   login: string
@@ -24,10 +24,8 @@ type PeriodType = {name: string; subjects: NoteSchema[]}
 type HorarySchema = {
   begin: string[]
   end: string[]
-  mon: string[]
-  tue: string[]
-  wed: string[]
-  thu: string[]
-  fri: string[]
-  sat: string[]
+  days: {item:string[]}[]
 }
+type SectionData = {title: string; data: string[]}[]
+type DicJSX = {[key:string]:JSX.Element}
+type strArr = {item:string[]}
